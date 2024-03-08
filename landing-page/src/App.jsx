f5c32049e6b23fc17e66741b4c1a36c6d03b2e36
rathodlakhan985@gmail.com <lakhanrrathod02@gmail.com>
2024-03-08 15:40:31 +0530

    With Little bit responsiveness

---
+++ landing-page/src/App.jsx
@@ -0,0 +1,15 @@
+import Navigation from './Components/Navigation';
+import Hero from './Components/Hero';
+import './App.css';
+
+function App() {
+
+   return (
+    <div>
+      <Navigation/>
+      <Hero/>
+    </div> 
+  )
+}
+
+export default App
---
+++ landing-page/src/Components/Hero.jsx
@@ -0,0 +1,27 @@
+export default function Hero(){
+    return (
+        <main className="hero container">
+            <div className="hero-content">
+                <h1>YOUR FEET DESERVE THE BEST</h1>
+                <p>
+                YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
+                </p>
+                <div className="hero-btn">
+                    <button>Shop Now</button>
+                    <button className="category">Category</button>
+                </div>
+                <div className="shopping">
+                    <p>Also Available On</p>
+                    <div className="brand-icons">
+                        <img src="/Images/amazon.png" alt="Amazon logo" />
+                        <img src="/Images/flipkart.png" alt="Flipkart Logo" />
+                    </div>
+                </div>
+            </div>
+            <div className="hero-image">
+                <img src="/Images/hero-image.png" alt="" />
+            </div>
+        </main>
+
+    )
+}
